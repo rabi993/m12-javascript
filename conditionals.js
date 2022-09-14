36888116c36e51614ceb95e2b6145e17097bc4ff
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2022-09-14 20:51:18 +0600

    multiple condition

--- conditionals.js
+++ conditionals.js
@@ -5,3 +5,43 @@ if(kolaPrice<10){
 else{
     console.log("khali cha better");
 }
+if(kolaPrice>10){
+    console.log( "khabona kola . ruti is better");
+}
+else{
+    console.log("khali cha better");
+}
+if(kolaPrice<=10){
+    console.log( "khabona kola . ruti is better");
+}
+else{
+    console.log("khali cha better");
+}
+if(kolaPrice===10){
+    console.log( "khabona kola . ruti is better");
+}
+else{
+    console.log("khali cha better");
+}
+if(kolaPrice!=10){
+    console.log( "khabona kola . ruti is better");
+}
+else{
+    console.log("khali cha better");
+}
+// hftgjtfghsdhflktrgfdfkl;jhguyttrftu;trtrirertyujihg
+var jobPaiso = true;
+var savingAmount= 5000;
+
+if (jobPaiso==true && savingAmount> 200000){
+    console.log("aso tomar patri khuji");
+}
+else{
+    console.log("tor kopale biye nai");
+}
+if (jobPaiso==true || savingAmount> 200000){
+    console.log("aso tomar patri khuji");
+}
+else{
+    console.log("tor kopale biye nai");
+}
