7982c5aab7d62b355511f85a599bb127f1d85437
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2022-09-14 17:23:20 +0600

    absolute,round,ceiling,,floor,random

--- math.js
+++ math.js
@@ -33,3 +33,25 @@ var p1='rabiul';
 var p2='islam';
 var total =p1+' '+p2;
 console.log(total);
+
+var number= -5;
+var absoluteNumber = Math.abs(number);
+console.log(absoluteNumber);
+var number= 5.3525;
+var result = Math.round(number);
+console.log(result);
+var number= 5.525;
+var result = Math.round(number);
+console.log(result);
+var number= 5.4925;
+var result = Math.ceil(number);
+console.log(result);
+var number= 5.4925;
+var result = Math.floor(number);
+console.log(result);
+var number= 25.4657;
+var result = Math.random();
+console.log(result);
+
+
+
